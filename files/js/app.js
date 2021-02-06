8b19e8ea552611574e4f8ed9e12eaaaad67e21de
AssADev <gravelteo.pro@gmail.com>
2021-02-06 17:49:07 +0100

    Drawing function

--- files/js/app.js
+++ files/js/app.js
@@ -1,11 +1,25 @@
 // Variables :
-let gridElementSelection = document.querySelectorAll(".grid_element");
+const optionsBtn = document.querySelectorAll(".toggle_options button");
+const inputColor = document.getElementById("color");
+const colorPicker = document.getElementById("color_pick");
+const toggleGrid = document.getElementById("grid_toggle");
+const clearBtn = document.getElementById("clear");
 
-// Slider :
 const inputRange = document.querySelector("input[type='range']");
 const progressBar = document.querySelector(".progress_bar");
 let gridSize = document.getElementById("grid_size");
 
+const gridContainer = document.querySelector(".grid_container");
+let gridElementSelection = document.querySelectorAll(".grid_element");
+
+let inkValue = "#6495ff";
+inputColor.value = inkValue;
+
+// Call begin functions :
+generateGrid(inputRange.value);
+updateInformations(inputRange.value);
+
+// Slider :
 inputRange.addEventListener("mousemove", () => updateInformations(inputRange.value));
 inputRange.addEventListener("change", () => generateGrid(inputRange.value));
 
@@ -14,9 +28,29 @@ function updateInformations(value) {
     gridSize.innerText = `${value} X ${value}`;
 }
 
-// Buttons :
-const optionsBtn = document.querySelectorAll(".toggle_options button");
+// Grid :
+function generateGrid(value) {
+    // Style the grid :
+    gridContainer.innerHTML = "";
+    gridContainer.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
 
+    // Create elements :
+    for (let i = 0; i < value ** 2; i++) {
+        const element = document.createElement("div");
+        element.classList.add("grid_element");
+        element.style.background = "#FFF";
+        if (toggleGrid.classList.contains("active")) {
+            element.classList.add("toggle_border");
+        }
+        gridContainer.appendChild(element);
+    }
+
+    // Assignment of all element :
+    gridElementSelection = document.querySelectorAll(".grid_element");
+}
+
+// Buttons :
 optionsBtn.forEach((optionBtn) =>
     optionBtn.addEventListener("click", function () {
         if (document.querySelector(".toggle_options button.active") !== null) {
@@ -43,12 +77,6 @@ optionsBtn.forEach((optionBtn) =>
 );
 
 // Color Input + Color Picker button :
-const inputColor = document.getElementById("color");
-const colorPicker = document.getElementById("color_pick");
-
-let inkValue = "#6495ff";
-inputColor.value = inkValue;
-
 inputColor.addEventListener("input", () => {
     inkValue = inputColor.value;
 });
@@ -63,49 +91,6 @@ colorPicker.addEventListener("click", () => {
             colorPicker.classList.remove("active");
         }
     });
-});
-
-// Grid Button :
-const toggleGrid = document.getElementById("grid_toggle");
-
-toggleGrid.addEventListener("click", () => {
-    toggleGrid.classList.toggle("active");
-    gridElementSelection.forEach((gridElement) => gridElement.classList.toggle("toggle_border"));
-});
-
-// Grid :
-const gridContainer = document.querySelector(".grid_container");
-
-function generateGrid(value) {
-    // Style the grid :
-    gridContainer.innerHTML = "";
-    gridContainer.style.gridTemplateRows = `repeat(${value}, 1fr)`;
-    gridContainer.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
-
-    // Create elements :
-    for (let i = 0; i < value ** 2; i++) {
-        const element = document.createElement("div");
-        element.classList.add("grid_element");
-        element.style.background = "#e0e6ff";
-        if (toggleGrid.classList.contains("active")) {
-            element.classList.add("toggle_border");
-        }
-        gridContainer.appendChild(element);
-    }
-
-    // Assignment of all element :
-    gridElementSelection = document.querySelectorAll(".grid_element");
-}
-
-// Call begin functions :
-generateGrid(inputRange.value);
-updateInformations(inputRange.value);
-
-// Clear Button :
-const clearBtn = document.getElementById("clear");
-
-clearBtn.addEventListener("click", () => {
-    gridElementSelection.forEach((gridElement) => (gridElement.style.backgroundColor = ""));
 });
 
 // Color Modification RGB to HEX :
@@ -129,3 +114,34 @@ function colorModification(rgb) {
     if (b.length == 1) b = "0" + b;
     return "#" + r + g + b;
 }
+
+// Grid Button :
+toggleGrid.addEventListener("click", () => {
+    toggleGrid.classList.toggle("active");
+    gridElementSelection.forEach((gridElement) => gridElement.classList.toggle("toggle_border"));
+});
+
+// Clear Button :
+clearBtn.addEventListener("click", () => {
+    gridElementSelection.forEach((gridElement) => (gridElement.style.background = ""));
+});
+
+// Drawing event :
+let movement = false;
+
+gridContainer.addEventListener("mousedown", (e) => {
+    movement = true;
+
+    if (!colorPicker.classList.contains("active")) {
+        e.target.style.backgroundColor = inkValue;
+
+        // For continus mousedown :
+        gridContainer.addEventListener("mousemove", (e) => {
+            if (movement) {
+                e.target.style.backgroundColor = inkValue;
+            }
+        });
+    }
+});
+
+gridContainer.addEventListener("mouseup", () => (movement = false));
